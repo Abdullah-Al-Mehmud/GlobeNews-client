de4674f05be10e7d255a72c43d51f61d9264d308
mehmud <timehmud121@gmail.com>
2023-11-30 04:25:17 +0600

    added subscription page and functionality

--- src/Pages/Subscription/Subscription.jsx
+++ src/Pages/Subscription/Subscription.jsx
@@ -1,5 +1,123 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../Provider/AuthProvider";
+
 const Subscription = () => {
-  return <div>subscription</div>;
+  const [allUsers, setAllUsers] = useState([]);
+  const { user } = useContext(AuthContext);
+  console.log(user);
+
+  const logedinUser = allUsers.find((users) => users?.email === user?.email);
+  console.log(logedinUser);
+
+  const [selectedPeriod, setSelectedPeriod] = useState("");
+
+  const handleSubscribe = () => {
+    if (!selectedPeriod) {
+      alert("Please select a subscription period");
+      return;
+    }
+
+    const currentDate = new Date();
+    let subscriptionEndDate;
+    switch (selectedPeriod) {
+      case "1min":
+        subscriptionEndDate = new Date(currentDate.getTime() + 1 * 60 * 1000);
+        break;
+      case "5days":
+        subscriptionEndDate = new Date(
+          currentDate.getTime() + 5 * 24 * 60 * 60 * 1000
+        );
+        break;
+      case "10days":
+        subscriptionEndDate = new Date(
+          currentDate.getTime() + 10 * 24 * 60 * 60 * 1000
+        );
+        break;
+
+      default:
+        return;
+    }
+
+    fetch(`http://localhost:3000/users/${logedinUser?._id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        premiumTaken: subscriptionEndDate,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+
+        // navigate("/payment");
+      })
+      .catch((error) => {
+        console.error("Error subscribing:", error);
+      });
+  };
+  useEffect(() => {
+    fetch("http://localhost:3000/users")
+      .then((res) => res.json())
+      .then((data) => setAllUsers(data));
+  }, []);
+  return (
+    <div>
+      <img src="https://i.ibb.co/HVQ91PM/4953844.jpg" alt="" />
+      <h1 className="text-center text-black font-bold   text-5xl uppercase pt-10">
+        special offer is Going on
+      </h1>
+      <h1 className="text-2xl text-red-500 text-center font-bold uppercase pt-5 pb-20">
+        ---Grab it now---
+      </h1>
+
+      <div className="rounded-xl">
+        <div
+          className="hero h-[200px] rounded-xl"
+          style={{
+            backgroundImage: "url(https://i.ibb.co/LrXGYft/7701.jpg)",
+          }}>
+          <div className="hero-overlay bg-opacity-60"></div>
+          <div className="hero-content text-center text-neutral-content rounded-xl">
+            <div className="">
+              <div className="flex flex-col md:flex-row items-center gap-5 ">
+                <select
+                  value={selectedPeriod}
+                  onChange={(e) => setSelectedPeriod(e.target.value)}
+                  className="select select-success w-[300px] md:w-[700px] text-main-blue-950 font-medium">
+                  <option value="">Select Subscription Period</option>
+                  <option value="1min">
+                    1 Minute{" "}
+                    <span className="text-lg text-main-blue-950 ml-10">
+                      $5{" "}
+                    </span>
+                  </option>
+                  <option value="5days">
+                    5 Days{" "}
+                    <span className="text-lg text-main-blue-950">$100 </span>
+                  </option>
+                  <option value="1week">
+                    1 Week{" "}
+                    <span className="text-lg text-main-blue-950">$500 </span>
+                  </option>
+                  <option value="1month">
+                    1 Month{" "}
+                    <span className="text-lg text-main-blue-950">$1000 </span>
+                  </option>
+                </select>
+                <button
+                  onClick={handleSubscribe}
+                  className="px-7 font-bold py-3 rounded-lg text-main-blue-50 bg-gradient-to-r from-[#6ba5ef] to-[#3367dd]  ">
+                  Subscribe Now
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Subscription;
